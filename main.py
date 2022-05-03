74de81111d6a5dce39bc1013ae55556e00111dc5
Etienne Springer <86609608+eddie0809@users.noreply.github.com>
2022-05-03 15:10:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -17,9 +17,8 @@ mpl.rcParams['font.family'] = ['serif']
 
 
 
+beta = 1
 
-
-beta = 1#/3000 # 1/(kb*300) ----> choose a value for beta then calculate the temperature from that, avoids overflow error (kb is small)
 #rho = np.exp(-beta * H0.diag())
 #Z = np.sum(rho)
 #rho = np.diag(rho / Z)
@@ -29,29 +28,47 @@ Z = rho.tr()
 rho = rho/Z
 
 
-initSystem = qeye(2)
+firstState = Qobj([[1,0],[0,0]])
+initSystem = firstState.unit()
 
 for n in range(N):
-	initSystem = tensor(initSystem, (-beta * sigmaz()).expm())
+	initSystem = tensor(initSystem, rho)
 
 
 t = np.linspace(0,np.pi,200)
 
-result = qutip.mesolve(Hges, initSystem, t, [], []).states
+result = qutip.mesolve(Hint, initSystem, t, [], []).states
 
 #myPlots.plotFidelity(initSystem, t, "Fidelity, $\\rho_0 = \dyad{1}$")#, 'Fidelityoneone')
 
 
+#print(result[-1].ptrace(N))
 
+# kullback leibler divergence
+qKLdiv = [entropy_relative(Qobj([[0,0],[0,1]]), result[dt].ptrace(N)) for dt in range(len(t))]
+myKLdiv = [entropy_relative(firstState.unit(), timeEvo(dt, initSystem, Hint).ptrace(N)) for dt in t]
+print(myKLdiv[-1])
+dumbHeat = [(myKLdiv[i+1] - myKLdiv[i])/beta for i in range(0,len(myKLdiv)-1)]
+myHeat = [1.j * (commutator(Hint, timeEvo(dt, initSystem, Hint)) * Hges).tr() for dt in t]
 
-myEntropy=[entropy_vn(timeEvo(dt, initSystem, Hges)) for dt in t]
-myIdot = deriv(t[1]-t[0], myEntropy)
-
-
-
-plt.plot(t[:-1], myIdot)
+fig, ax = plt.subplots()
+ax.plot(t, myKLdiv, label="$\mathcal{D}_\\text{KL}(\\rho^1(0)||\\rho^N(t))$")
+#plt.title("Kullback-Leibler Divergence from $\\rho^1(0)$ to $\\rho^N(t)$")
+#plt.plot(t[:-1], deriv(t[2]-t[1], myKLdiv))
+ax.set_xlabel("time")
+#plt.ylabel("$\mathcal{D}_\\text{KL}$")
+#plt.show()
+ax.plot(t[:-1], deriv(t[2]-t[1], myKLdiv), label="$\dot{\mathcal{D}}_\\text{KL}$")
+#plt.title("Information flow from $\\rho^1(0)$ to $\\rho^N(t)$")
+#plt.xlabel("time")
+#plt.ylabel("$\dot{\mathcal{D}}_\\text{KL}$")
+#plt.show()
+ax.plot(t, np.real(myHeat), label="$\dot{\mathcal{Q}}$")
+ax.plot(t[:-1], dumbHeat)
+ax.set_title("$\mathcal{D}_\\text{KL}, \dot{\mathcal{D}}_\\text{KL}$ and $\dot{\mathcal{Q}}$ from $\\rho^1(0)= \dyad{0}$ to $\\rho^N(t)$")
+#plt.xlabel("time")
+#plt.ylabel("$\dot{Q}_\\text{KL}$")
+ax.legend()
+ax.axhline(np.pi/3 * beta,color='grey')
+ax.axhline(0,color='grey')
 plt.show()
-
-plt.close('all')
-
-
